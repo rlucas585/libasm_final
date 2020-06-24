1c58033af1cc5b552355f85eee6b67f1fce9c851
Ryan Lucas <rlucas@f0r4s6.codam.nl>
2020-06-24 12:51:43 +0200

    Revert "Updated ft_read and ft_write"
    This reverts commit 7607bb4bf1dbe2ae41debc70cb314aa3c3fbd59e.
    
    Turns out you are meant to submit a main with your project, so that's a
    fail.

--- src/ft_read.s
+++ src/ft_read.s
@@ -6,7 +6,7 @@
 #    By: rlucas <marvin@codam.nl>                     +#+                      #
 #                                                    +#+                       #
 #    Created: 2020/02/20 10:39:44 by rlucas        #+#    #+#                  #
-#    Updated: 2020/06/23 13:25:28 by rlucas        ########   odam.nl          #
+#    Updated: 2020/02/28 17:18:51 by rlucas        ########   odam.nl          #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,18 +16,11 @@
 ; ssize_t	ft_read(int fildes, void *buf, size_t nbyte);
 
 			global		_ft_read
-			extern		___error
 
-_ft_read:	push		rbx
-			mov			rax, 0x2000003
+_ft_read:	mov			rax, 0x2000003
 			syscall
 			jc			err
-			pop			rbx
 			ret
 
-err:		mov			rbx, rax
-			call		___error
-			mov			[rax], rbx
-			mov			rax, -0x01
-			pop			rbx
+err:		mov			rax, -0x01
 			ret
--- src/ft_write.s
+++ src/ft_write.s
@@ -6,7 +6,7 @@
 #    By: rlucas <marvin@codam.nl>                     +#+                      #
 #                                                    +#+                       #
 #    Created: 2020/02/19 15:56:05 by rlucas        #+#    #+#                  #
-#    Updated: 2020/06/23 13:34:05 by rlucas        ########   odam.nl          #
+#    Updated: 2020/02/28 17:27:46 by rlucas        ########   odam.nl          #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,18 +16,11 @@
 ; ssize_t	ft_write(int fildes, void *buf, size_t nbyte);
 
 			global		_ft_write
-			extern		___error
 
-_ft_write:	push		rbx
-			mov			rax, 0x2000004
+_ft_write:	mov			rax, 0x2000004
 			syscall
 			jc			err
-			pop			rbx
 			ret
 
-err:		mov			rbx, rax
-			call		___error
-			mov			[rax], rbx
-			mov			rax, -0x01
-			pop			rbx
+err:		mov			rax, -0x01
 			ret
